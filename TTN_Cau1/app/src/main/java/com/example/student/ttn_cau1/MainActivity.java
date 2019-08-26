44b339e057b758d6b6eaa1c078ec8acf004d5712
trantrungnam1997vn3 <trantrungnam1997vn3>
2019-08-26 13:25:37 +0700

    Add function handle sum

---
+++ TTN_Cau1/app/src/main/java/com/example/student/ttn_cau1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.student.ttn_cau1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnTinhTong;
+    private EditText inputX, inputY;
+    private TextView textKetQua;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnTinhTong = (Button) findViewById(R.id.btnTinhTong);
+
+    }
+
+    public void tinhTong(View view) {
+        inputX = (EditText) findViewById(R.id.inputX);
+        inputY = (EditText) findViewById(R.id.inputY);
+        int x = Integer.parseInt(inputX.getText().toString());
+        int y = Integer.parseInt(inputY.getText().toString());
+
+        int ketqua = x + y;
+
+        textKetQua = (TextView) findViewById(R.id.textKetQua);
+        textKetQua.setText("Tổng là: " + ketqua);
+
+
+    }
+}
